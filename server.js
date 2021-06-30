7657b934867d4e6531e90d63bf6cc071e4c074ba
ShareeO <selena74354@yahoo.com>
2021-06-30 18:44:32 -0500

    initail server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+
+})
+
+app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT)
+})
