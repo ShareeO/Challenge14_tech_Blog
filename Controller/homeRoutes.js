b84d6149a75f42d4ef46fee603a45734b22c0130
ShareeO <selena74354@yahoo.com>
2021-06-30 21:26:26 -0500

    setup handlebars

--- Controller/homeRoutes.js
+++ Controller/homeRoutes.js
@@ -8,7 +8,14 @@ router.get("/", (req,res) =>{
     res.send("Hello from router")
 })
 
+router.get("/login", (req, res) => {
 
+    res.render("login")
+})
+
+router.get("/signup", (req, res) => {
+
+})
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,8 +8,8 @@ const app = express();
 const sequelize = require("./config/connection");
 
 
-
-
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
