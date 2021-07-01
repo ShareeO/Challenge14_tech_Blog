828c7b1e22867f5919db48d8952e19c3659180cc
ShareeO <selena74354@yahoo.com>
2021-06-30 20:47:01 -0500

    require controller into server

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-const sequelize = require("./config/connection")
+const sequelize = require("./config/connection");
 
-app.get("/", (req, res) => {
 
-})
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(require("./controller"));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log("App listening on PORT " + PORT));
