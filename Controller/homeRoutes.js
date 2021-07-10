bd4856b747953aad81d2e607940dd55e5d745779
ShareeO <selena74354@yahoo.com>
2021-07-10 17:34:37 -0500

    home routes

--- Controller/homeRoutes.js
+++ Controller/homeRoutes.js
@@ -1,21 +1,60 @@
 const router = require("express").Router();
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
-router.get("/", (req,res) =>{
+router.get("/", (req, res) => {
+    Post.findAll({
+    attributes: [
+        "id",
+        "post_url",
+        "title",
+        "created_at",
+        [
+        sequelize.literal(
+            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+        ],
+    ],
+    include: [
+        {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+            model: User,
+            attributes: ["username"],
+        },
+        },
+        {
+        model: User,
+        attributes: ["username"],
+        },
+    ],
+    })
+    .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
 
-
-
-    res.render("home")
-})
+        res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 router.get("/login", (req, res) => {
-
-    res.render("login")
-})
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+        }
+    
+        res.render('login');
+    });
 
 router.get("/signup", (req, res) => {
-
-})
-
+    res.render('signup');
+});
 
 module.exports = router;
